0ce984466c78b3ae4c97664f9c21c6b49bb52655
Guada18 <93491131+Guada18@users.noreply.github.com>
2023-04-14 18:45:20 -0300

    Update Bino.js

--- Bino/Bino.js
+++ Bino/Bino.js
@@ -32,33 +32,33 @@ function ContarMultiplosDe5(x) {
 
 function DetectarMultiplos(arr, amount, n) {
 
-    for( i = 0; i<n; i++ ){
-        if(ContarMultiplosDe2(arr[i]) == true){
+    for (i = 0; i < n; i++) {
+        if (ContarMultiplosDe2(arr[i]) == true) {
             amount[0]++;
         }
-        if(ContarMultiplosDe3(arr[i]) == true){
+        if (ContarMultiplosDe3(arr[i]) == true) {
             amount[1]++;
         }
-        if(ContarMultiplosDe4(arr[i]) == true){
+        if (ContarMultiplosDe4(arr[i]) == true) {
             amount[2]++;
         }
-        if(ContarMultiplosDe5(arr[i]) == true){
+        if (ContarMultiplosDe5(arr[i]) == true) {
             amount[3]++;
         }
     }
-    
+
     return amount;
 }
 
 function print(amount, arr, n) {
-    document.write('<table ><tr style="border: 1px solid black><td><p>Entrada</p></td><td><p>Salida</p></td></tr>');
-    document.write('<tr><td><p ;">' + n + '</p></br><p style="border: 1px solid black;">');
+    document.write('<table border = "1px"><tr><td><p>Entrada</p></td><td><p>Salida</p></td></tr>');
+    document.write('<tr><td><p>' + n + '</p></br><p>');
     for (i = 0; i < n; i++) {
         document.write(' ' + arr[i] + ' ');
     }
     document.write('</p></td><td>');
     for (i = 2; i <= 5; i++) {
-        document.write('<p>'+ amount[i - 2] +' Multiplos de ' + i + '</p>');
+        document.write('<p>' + amount[i - 2] + ' Multiplos de ' + i + '</p>');
     }
     document.write('</td></tr></table>');
 }
